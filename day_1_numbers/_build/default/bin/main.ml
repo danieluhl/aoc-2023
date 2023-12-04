81f38a5712143eafde8e46544e906f7fd892ecb3
Dan Uhl <dan.uhl@posh.tech>
2023-12-04 07:12:44 -0500

    day 1 part 2 still not working but seems right

---
+++ day_1_numbers/_build/default/bin/main.ml
@@ -0,0 +1,102 @@
+open Core
+
+type search_num =
+  { str : string
+  ; ch : string
+  }
+
+let search_num_list =
+  [ { str = "one"; ch = "1" }
+  ; { str = "two"; ch = "2" }
+  ; { str = "three"; ch = "3" }
+  ; { str = "four"; ch = "4" }
+  ; { str = "five"; ch = "5" }
+  ; { str = "six"; ch = "6" }
+  ; { str = "seven"; ch = "7" }
+  ; { str = "eight"; ch = "8" }
+  ; { str = "nine"; ch = "9" }
+  ]
+;;
+
+let find_substring_left str substr =
+  try
+    let regex = Str.regexp_string substr in
+    let index = Str.search_forward regex str 0 in
+    Some index
+  with
+  | _ -> None
+;;
+
+let find_substring_right str substr =
+  try
+    let regex = Str.regexp_string substr in
+    let index = Str.search_backward regex str (String.length str) in
+    Some index
+  with
+  | _ -> None
+;;
+
+type min_record =
+  { ch : string
+  ; i : int
+  }
+
+(* get index of first number *)
+let get_left_number line =
+  List.fold search_num_list ~init:{ ch = ""; i = Int.max_value } ~f:(fun acc x ->
+    let left_ch_i =
+      match find_substring_left line x.ch with
+      | Some x -> x
+      | _ -> Int.max_value
+    in
+    let left_str_i =
+      match find_substring_left line x.str with
+      | Some x -> x
+      | _ -> Int.max_value
+    in
+    match acc with
+    | { i; _ } when left_ch_i < i && left_ch_i < left_str_i ->
+      { ch = x.ch; i = left_ch_i }
+    | { i; _ } when left_str_i < i -> { ch = x.ch; i = left_str_i }
+    | _ -> acc)
+;;
+
+let get_right_number line =
+  List.fold search_num_list ~init:{ ch = "0"; i = 0 } ~f:(fun acc x ->
+    let right_ch_i =
+      match find_substring_right line x.ch with
+      | Some x -> x
+      | _ -> -1
+    in
+    let right_str_i =
+      match find_substring_right line x.str with
+      | Some x -> x
+      | _ -> -1
+    in
+    match acc with
+    | { i; _ } when right_ch_i > i && right_ch_i > right_str_i ->
+      { ch = x.ch; i = right_ch_i }
+    | { i; _ } when right_str_i > i -> { ch = x.ch; i = right_str_i }
+    | _ -> acc)
+;;
+
+let () =
+  let lines = Day_1_numbers.Read.read_file "input.txt" in
+  let result =
+    List.fold
+      ~init:0
+      ~f:(fun acc line ->
+        let { ch = left_ch; i = left_i } = get_left_number line in
+        let { ch = right_ch; i = right_i } = get_right_number line in
+        Printf.printf "\n%d %d" left_i right_i;
+        if left_i = right_i
+        then (
+          Printf.printf "\n+%s" left_ch;
+          acc + int_of_string left_ch)
+        else (
+          Printf.printf "\n+%s%s" left_ch right_ch;
+          acc + int_of_string (left_ch ^ right_ch)))
+      lines
+  in
+  Printf.printf "\nresult: %d\n" result
+;;
---
+++ day_1_numbers/_build/default/lib/read.ml
@@ -0,0 +1,20 @@
+(* let rec concat_lines acc = *) 
+(*   match acc with *) 
+(*   | [] -> "" *)
+(*   | h :: t -> h ^ "\n" ^ concat_lines t *)
+
+let read_file filename = 
+  let file = open_in filename in
+  let rec read_lines acc =
+    try
+      let line = input_line file in
+      read_lines (line :: acc)
+    with
+    | End_of_file -> List.rev acc
+  in
+  let lines = read_lines [] in
+  close_in file;
+  lines
+
+
+
